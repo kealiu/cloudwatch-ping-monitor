8d64c1a718a3e08a8ca07efd577f37dda87d148b
Ubuntu <ubuntu@ip-172-31-36-172.ap-northeast-1.compute.internal>
2021-12-06 02:00:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cloudwatch Ping Monitor
+
+collect ping status with high resolution, and send it to cloudwatch.
+
+# usage
+
+you can launch a spot auto scaling group, and make a composited alarm for network status change.
+
+
---
+++ monitor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+# SSM Parameter Store config smaple
+#TARGETIPS=172.31.36.54,52.83.24.131,172.31.36.110
+#NAMESPACE=DXPingMonitor
+#METRICNAME=OK
+#TIMEOUTSEC=0.005
+
+# for use watch, export all these values as envvar
+SSHPMSTORE=PingMintorConfig
+
+for values in $(aws ssm get-parameter --name ${SSHPMSTORE} | jq -r ".Parameter.Value")
+do
+    export $values
+done
+
+function cloudwatchPutMetric() {
+    aws cloudwatch put-metric-data --metric-name ${METRICNAME} --namespace ${NAMESPACE} --unit None --value $2 --dimensions TargetIP=$1,InstanceIP=${INSTANCEIP} --storage-resolution 1
+}
+
+function cloudwatchOneIPping() {
+    timeout ${TIMEOUTSEC} ping -n -c 1 $1 2>&1 > /dev/null
+    if [ $? -eq 0 ] ; then
+        cloudwatchPutMetric $1 1
+    else
+	cloudwatchPutMetric $1 0
+    fi
+}
+
+export -f cloudwatchPutMetric
+export -f cloudwatchOneIPping
+
+for targetip in $(echo $TARGETIPS | sed 's/,/ /g')
+do
+    nohup watch -n 1 -x bash -c "cloudwatchOneIPping ${targetip}" 2>&1 > /dev/null & 
+done
